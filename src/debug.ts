ce7c663de9f47fc7c7a67a8d420a732a66627d3b
Neal Beeken <neal.beeken@mongodb.com>
2020-06-11 20:34:56 -0400

    Update dependencies and lint

--- src/debug.ts
+++ src/debug.ts
@@ -85,7 +85,7 @@ export class DashmipsDebugSession extends LoggingDebugSession {
     }
 
     protected async sleep(ms: number) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+        return new Promise(resolve => setTimeout(resolve, ms))
     }
 
     protected async initializeRequest(
@@ -114,20 +114,18 @@ export class DashmipsDebugSession extends LoggingDebugSession {
     protected async launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments) {
         this.config = args
         this.runInTerminalRequest(...buildTerminalLaunchRequestParams(args))
-        await this.sleep(1000);
-
+        await this.sleep(1000)
 
         this.client.connect(args.host, args.port)
         this.client.open.notifyAll()
         await this.client.verified.wait(0)
 
         this.client.call('start')
-        this.client.once('start', pid => {
+        this.client.once('start', (pid) => {
             this.client.dashmipsPid = pid.pid
             if (this.config.stopOnEntry) {
                 this.sendEvent(new StoppedEvent('entry', THREAD_ID))
-            }
-            else if (this.client.stopEntry) {
+            } else if (this.client.stopEntry) {
                 this.sendEvent(new StoppedEvent('breakpoint', THREAD_ID))
             }
             else { this.client.call('continue', this.breakpoints) }
@@ -140,7 +138,7 @@ export class DashmipsDebugSession extends LoggingDebugSession {
         this.client.connect(args.host, args.port)
         this.client.open.notifyAll()
         this.client.call('start')
-        this.client.once('start', pid => {
+        this.client.once('start', (pid) => {
             this.client.dashmipsPid = pid.pid
             if (this.config.stopOnEntry) {
                 this.sendEvent(new StoppedEvent('entry', THREAD_ID))
@@ -182,7 +180,7 @@ export class DashmipsDebugSession extends LoggingDebugSession {
                             // -1 indicates an unverified breakpoints (not a line of MIPS code)
                             locations[idx] != -1,
                             bp.line,
-                            bp.column,
+                            bp.column
                         )
                 ),
             }
@@ -219,7 +217,7 @@ export class DashmipsDebugSession extends LoggingDebugSession {
                 },
             ]
             response.body = {
-                stackFrames: stack.map(f => {
+                stackFrames: stack.map((f) => {
                     return new StackFrame(
                         f.index,
                         f.name,
